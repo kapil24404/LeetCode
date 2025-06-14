ed58849120d77dabc20989f3ad5e24a72d67eaab
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-06-14 13:56:24 +0530

    [242] [Time Beats: 19.39%] [Memory Beats: 49.56%] - LeetPush

---
+++ Valid-Anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+           hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+           if (!hm.containsKey(ch)) {
+                return false;
+            }
+            hm.put(ch, hm.get(ch) - 1);
+            if (hm.get(ch) < 0) {
+                return false;
+            }
+        }
+        for(Map.Entry<Character,Integer> ans:hm.entrySet()){
+            if(ans.getValue()!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
