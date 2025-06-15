fb3a77ab1914f17171dc17aab94c2b09578e5457
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-06-15 07:17:56 +0530

    [1662] [Time Beats: 15.38%] [Memory Beats: 97.01%] - LeetPush

--- Check-If-Two-String-Arrays-are-Equivalent.java
+++ Check-If-Two-String-Arrays-are-Equivalent.java
@@ -1,13 +1,31 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder str1=new StringBuilder();
-        StringBuilder str2=new StringBuilder();
-        for(int i=0;i<word1.length;i++){
-            str1.append(word1[i]);
+        // StringBuilder str1=new StringBuilder();
+        // StringBuilder str2=new StringBuilder();
+        // for(int i=0;i<word1.length;i++){
+        //     str1.append(word1[i]);
+        // }
+        // for(int i=0;i<word2.length;i++){
+        //     str2.append(word2[i]);
+        // }
+        // return str1.toString().equals(str2.toString());
+
+        int i=0,j=0,w1=0,w2=0;
+        int m=word1.length,n=word2.length;
+        while(w1<m&& w2<n){
+            if(word1[w1].charAt(i)!=word2[w2].charAt(j)){
+                return false;
+            }
+            i++; j++;
+            if(i==word1[w1].length()){
+                i=0;
+                w1++;
+            }
+            if(j==word2[w2].length()){
+                j=0;
+                w2++;
+            }
         }
-        for(int i=0;i<word2.length;i++){
-            str2.append(word2[i]);
-        }
-        return str1.toString().equals(str2.toString());
+        return w1 == word1.length && w2 == word2.length;
     }
 }
