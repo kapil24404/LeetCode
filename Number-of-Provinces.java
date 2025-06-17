24dbfbdb79ad936490603ba86fc0fd99b33674e8
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-06-17 10:34:21 +0530

    [547] [Time Beats: 88.49%] [Memory Beats: 21.52%] - LeetPush

--- Number-of-Provinces.java
+++ Number-of-Provinces.java
@@ -1,34 +1,52 @@
 class Solution {
+    int n;
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
+         n = isConnected.length;
+        //ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // for (int i = 0; i < n; i++) {
+        //     adj.add(new ArrayList<>());
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (isConnected[i][j] == 1) {
+        //             adj.get(i).add(j);
+        //         }
+        //     }
+        // }
+        boolean[] vis = new boolean[n];
+        int count = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 ) {
-                    adj.get(i).add(j);
-                }
-            }
-        }
-        boolean vis[]=new boolean[n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                DFS(adj,i,vis);
+            if (!vis[i]) {
+                BFS(isConnected, i, vis);
                 count++;
             }
         }
         return count;
     }
-    public void DFS(ArrayList<ArrayList<Integer>> adj,int u,boolean[] vis){
-            if(vis[u]) return ;
-            vis[u]=true;
-            for(int v:adj.get(u)){
-                if(!vis[v]){
-                    DFS(adj,v,vis);
+
+    public void BFS(int[][] isConnected, int u, boolean[] vis) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(u);
+        vis[u] = true;
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            for (int v=0;v<n;v++) {
+                if (!vis[v] && isConnected[curr][v]==1) {
+                    vis[v] = true;
+                    q.add(v);
                 }
             }
         }
+    }
 }
+
+    // public void DFS(ArrayList<ArrayList<Integer>> adj,int u,boolean[] vis){
+    //         if(vis[u]) return ;
+    //         vis[u]=true;
+    //         for(int v:adj.get(u)){
+    //             if(!vis[v]){
+    //                 DFS(adj,v,vis);
+    //             }
+    //         }
+    //     }
+  
