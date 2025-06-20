209e61189de5d6efe1b542a29be7d601cd1c26e7
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-06-20 16:05:22 +0530

    [42] [Time Beats: 62.74%] [Memory Beats: 74.08%] - LeetPush

--- Trapping-Rain-Water.java
+++ Trapping-Rain-Water.java
@@ -1,25 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        if (height == null || height.length < 3) {
-            return 0;
+        int n=height.length;
+        int leftmax[]=new int[n];
+        int rightmax[]=new int[n];
+        leftmax[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(height[i],leftmax[i-1]);
         }
-
-        int n = height.length;
-        int[] leftMax = new int[n];
-        int[] rightMax = new int[n];
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        rightmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(height[i],rightmax[i+1]);
         }
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=Math.min(leftmax[i],rightmax[i])-height[i];
         }
-        int totalWater = 0;
-        for (int i = 0; i < n; i++) {
-            totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
-        }
-
-        return totalWater;
+        return ans;
     }
 }
