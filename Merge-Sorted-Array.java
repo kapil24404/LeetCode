0b82d7e83ed1a0d72bb488d158dc815b0dd1afb2
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-06-28 11:47:28 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 42.60%] - LeetPush

--- Merge-Sorted-Array.java
+++ Merge-Sorted-Array.java
@@ -1,28 +1,51 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=0;
-        int j=0,k=0;
-        int temp[]=new int[m+n];
-        while(i<m && j<n){
+
+
+        //****************WITH EXTRA SPACE */
+        // int i=0;
+        // int j=0,k=0;
+        // int temp[]=new int[m+n];
+        // while(i<m && j<n){
+        //     if(nums1[i]<=nums2[j]){
+        //         temp[k]=nums1[i];
+        //         k++;
+        //         i++;
+        //     }
+        //     else{
+        //         temp[k]=nums2[j];
+        //         j++;
+        //         k++;
+        //     }
+        // }
+        // while(i<m){
+        //     temp[k++]=nums1[i++];
+        // }
+        // while(j<n){
+        //     temp[k++]=nums2[j++];
+        // }
+        // for(int t=0;t<k;t++){
+        //     nums1[t]=temp[t];
+        // }
+
+        //****************WITHOUT EXTRA SPACE */
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
             if(nums1[i]<=nums2[j]){
-                temp[k]=nums1[i];
-                k++;
-                i++;
+                nums1[k--]=nums2[j--];
             }
             else{
-                temp[k]=nums2[j];
-                j++;
-                k++;
+                nums1[k--]=nums1[i--];
             }
         }
-        while(i<m){
-            temp[k++]=nums1[i++];
+        while(i>=0){
+            nums1[k--]=nums1[i--];
         }
-        while(j<n){
-            temp[k++]=nums2[j++];
+         while(j>=0){
+            nums1[k--]=nums2[j--];
         }
-        for(int t=0;t<k;t++){
-            nums1[t]=temp[t];
-        }
+
     }
 }
