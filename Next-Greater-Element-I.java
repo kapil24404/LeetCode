93803a52d14f4ddc9204921406744cf09db8b6b4
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-05 12:28:42 +0530

    [496] [Time Beats: 92.76%] [Memory Beats: 39.97%] - LeetPush

--- Next-Greater-Element-I.java
+++ Next-Greater-Element-I.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> st=new Stack<>();
-        HashMap<Integer,Integer> hm=new HashMap<>();
+        Map<Integer,Integer> hm=new HashMap<>();
         int n=nums2.length;
-        int[] res=new int[n];
-        res[n-1]=-1;
         st.push(nums2[n-1]);
+        int res[]=new int[n];
+        res[n-1]=-1;
         for(int i=n-2;i>=0;i--){
-            while(!st.isEmpty()&& nums2[i]>=st.peek()){
+            while(!st.isEmpty() &&nums2[i]>=st.peek()){
                 st.pop();
             }
             res[i]=st.isEmpty()?-1:st.peek();
             st.push(nums2[i]);
         }
-        for(int i=0;i<res.length;i++){
+        for(int i=0;i<n;i++){
             hm.put(nums2[i],res[i]);
         }
         for(int i=0;i<nums1.length;i++){
@@ -22,4 +22,3 @@ class Solution {
         return nums1;
     }
 }
-
