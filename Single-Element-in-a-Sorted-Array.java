436ffa7cb43179516337498e66142af65b373dac
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-05 12:02:39 +0530

    [540] [Time Beats: 25.39%] [Memory Beats: 80.18%] - LeetPush

--- Single-Element-in-a-Sorted-Array.java
+++ Single-Element-in-a-Sorted-Array.java
@@ -1,17 +1,22 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-     int start=0;
-     int end=nums.length-1;
-     while(start<end){
-        int mid=start+(end-start)/2;
-        if (mid % 2 == 1) mid--;
-        if( nums[mid]==nums[mid+1]){
-            start=mid+2;
-        }
-        else{
-            end=mid;
-        }
-     }   
-     return nums[start];
+    //  int start=0;
+    //  int end=nums.length-1;
+    //  while(start<end){
+    //     int mid=start+(end-start)/2;
+    //     if (mid % 2 == 1) mid--;
+    //     if( nums[mid]==nums[mid+1]){
+    //         start=mid+2;
+    //     }
+    //     else{
+    //         end=mid;
+    //     }
+    //  }   
+    //  return nums[start];
+    int ans=0;
+    for (int num : nums) {
+        ans ^= num;
+    }
+    return ans;
     }
 }
