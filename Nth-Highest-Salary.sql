1a17899368d2e42b9a9d691e5769dd47a619aef8
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-09 13:18:24 +0530

    [177] [Time Beats: 51.27%] - LeetPush

---
+++ Nth-Highest-Salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+ DECLARE offsetVal INT;
+  SET offsetVal = N - 1;
+  RETURN (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET offsetval
+  );
+END
