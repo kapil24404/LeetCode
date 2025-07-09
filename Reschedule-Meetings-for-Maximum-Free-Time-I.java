bf1e10c8c9237eea62cd73376186fbf95e7568de
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-09 10:51:09 +0530

    [3439] [Time Beats: 76.11%] [Memory Beats: 6.67%] - LeetPush

--- Reschedule-Meetings-for-Maximum-Free-Time-I.java
+++ Reschedule-Meetings-for-Maximum-Free-Time-I.java
@@ -1,15 +1,24 @@
-public class Solution {
+class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
-        int n = startTime.length, res = 0;
-        int[] sum = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            sum[i + 1] = sum[i] + endTime[i] - startTime[i];
+        int n = startTime.length;
+        long[] gaps = new long[n + 1];
+        gaps[0] = startTime[0];
+        gaps[n] = eventTime - endTime[n - 1];
+
+        for(int i = 1; i < n; i++) {
+            gaps[i] = startTime[i] - endTime[i - 1];
         }
-        for (int i = k - 1; i < n; i++) {
-            int right = i == n - 1 ? eventTime : startTime[i + 1];
-            int left = i == k - 1 ? 0 : endTime[i - k];
-            res = Math.max(res, right - left - (sum[i + 1] - sum[i - k + 1]));
+
+        long[] prefix = new long[n + 2];
+        for(int i = 1; i <= n + 1; i++) {
+            prefix[i] = prefix[i - 1] + gaps[i - 1];
         }
-        return res;
+
+        long maxFreeTime = 0;
+        for(int i = k + 1; i <= n + 1; i++) {
+            maxFreeTime = Math.max(maxFreeTime, prefix[i] - prefix[i - (k + 1)]);
+        }
+
+        return (int)maxFreeTime;
     }
 }
