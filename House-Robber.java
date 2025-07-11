1df370a89ef833e7b2b985e8051939aebfc358d2
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-11 17:26:55 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 46.33%] - LeetPush

---
+++ House-Robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(0,nums,dp);
+    }
+    public int helper(int i,int nums[],int dp[]){
+        if(i>=nums.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+        dp[i]=Math.max(nums[i]+helper(i+2,nums,dp),helper(i+1,nums,dp));
+        return dp[i];
+    }
+}
