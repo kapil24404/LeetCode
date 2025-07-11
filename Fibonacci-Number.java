6428367d7ef55faf8c30a72ffd46a059fcd7a38b
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-11 08:55:06 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 49.67%] - LeetPush

---
+++ Fibonacci-Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        int dp[]=new int[n+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i]=-1;
+        }
+        return fibo(n,dp);
+    }
+    public int fibo(int n,int dp[]){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        else
+        return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    }
+}
