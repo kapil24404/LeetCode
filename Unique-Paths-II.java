eb39691d212266951d7d3933e9bb0249e1703052
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-14 17:28:41 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 49.64%] - LeetPush

--- Unique-Paths-II.java
+++ Unique-Paths-II.java
@@ -2,21 +2,20 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        if (obstacleGrid[0][0] == 1||obstacleGrid[m - 1][n - 1] == 1) return 0;
         int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
+        for(int rows[]:dp){
+            Arrays.fill(rows,-1);
         }
-        return helper(m -1,n-1,obstacleGrid,dp);
+        return helper(m-1,n-1,obstacleGrid,dp);
     }
-     public int helper(int m,int n,int obstacleGrid[][],int dp[][]){
-        if(m<0 ||n<0) return 0;
-        if(m==0 && n==0) return 1;
-        if (obstacleGrid[m][n] == 1) return 0;
+     public static int helper(int m,int n,int nums[][],int dp[][]){
+        if(m<0 || n<0) return 0;
+        if(nums[m][n]==1) return 0;
+        if(m == 0 && n == 0) return nums[0][0] == 1 ? 0 : 1;
         if(dp[m][n]!=-1) return dp[m][n];
-        dp[m][n]=helper(m-1,n,obstacleGrid,dp)+helper(m,n-1,obstacleGrid,dp);
+        int left=helper(m,n-1,nums,dp);
+        int up=helper(m-1,n,nums,dp);
+        dp[m][n]=up+left;
         return dp[m][n];
     }
 }
