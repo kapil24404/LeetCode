077e600319cdb3bc86814d5c8649d1934794394d
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-16 18:48:40 +0530

    [3] [Time Beats: 88.88%] [Memory Beats: 69.62%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.java
+++ Longest-Substring-Without-Repeating-Characters.java
@@ -1,16 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0) return 0;
+        int n=s.length();
+        int maxlen=0;
         int left=0;
         HashMap<Character,Integer> hm=new HashMap<>();
-        int maxlen=Integer.MIN_VALUE;
-        for(int right=0;right<s.length();right++){
+        for(int right=0;right<n;right++){
             char ch=s.charAt(right);
             if(hm.containsKey(ch)){
-              left=Math.max(left,hm.get(ch)+1);  
+                left = Math.max(left, hm.get(ch) + 1);
+                
             }
             hm.put(ch,right);
-            maxlen=Math.max(maxlen,right-left+1);
+                maxlen=Math.max(maxlen,right-left+1);
         }
         return maxlen;
     }
