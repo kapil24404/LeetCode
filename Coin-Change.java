ece83510ce0dfd110223cc681b461f71d9fd2410
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-19 22:06:06 +0530

    [322] [Time Beats: 24.90%] [Memory Beats: 21.55%] - LeetPush

--- Coin-Change.java
+++ Coin-Change.java
@@ -1,27 +1,29 @@
-// 
-
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        int INF = (int) 1e9;
-
-        // base cases
-        for (int i = 0; i <= n; i++) dp[i][0] = 0;
-        for (int j = 1; j <= amount; j++) dp[0][j] = INF;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    // include current coin (same i because unbounded)
-                    dp[i][j] = Math.min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
-                } else {
-                    // exclude current coin
-                    dp[i][j] = dp[i - 1][j];
-                }
+        int n=coins.length;
+        int dp[][]=new int[n][amount+1];
+         for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        int result= helper(n-1,coins,amount,dp);
+        return result >= (int)1e9 ? -1 : result;
+    }
+    public int helper(int i,int coins[],int amount,int dp[][]){
+        if(i==0){
+            if (amount % coins[0] == 0) {
+                return amount / coins[0];
+            } else {
+                return (int)1e9 ;
             }
         }
-
-        return dp[n][amount] >= INF ? -1 : dp[n][amount];
+        if (dp[i][amount] != -1) return dp[i][amount];
+        int n=coins.length;
+        int nottake=helper(i-1,coins,amount,dp);
+        int take=Integer.MAX_VALUE;
+        if(coins[i]<=amount){
+            take=1+helper(i,coins,amount-coins[i],dp);
+        }
+        dp[i][amount]=Math.min(nottake,take);
+        return dp[i][amount];
     }
 }
