21c1c0cc2985a84c56d169fc8472e71c05fcbd3b
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-20 18:58:53 +0530

    [104] [Time Beats: 100.00%] [Memory Beats: 95.59%] - LeetPush

--- Maximum-Depth-of-Binary-Tree.java
+++ Maximum-Depth-of-Binary-Tree.java
@@ -15,15 +15,7 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        int left=maxDepth(root.left);
-        int right=maxDepth(root.right);
-        // if(root.left==null&&root.right==null){
-        //     return 0;
-        // }
-        int height=1+Math.max(left,right);
-        return height;
+        if(root==null) return 0;
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 }
