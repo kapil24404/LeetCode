7879e80bc6db67dd384b25cb2d2047e85728d882
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-25 21:40:52 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 27.50%] - LeetPush

--- Climbing-Stairs.java
+++ Climbing-Stairs.java
@@ -1,25 +1,14 @@
 class Solution {
-    // public int climbStairs(int n) {
-    //     int dp[]=new int[n+1];
-    //     Arrays.fill(dp,-1);
-    //     return helper(n,dp);
-    // }
-    // public int helper(int i,int dp[]){
-    //     if(i==0 ||i==1) return 1;
-    //    // if(i<0) return 0;
-    //     if(dp[i]!=-1) return dp[i];
-    //     else dp[i]=helper(i-1,dp)+helper(i-2,dp);
-    //     return dp[i];
-    // }
-    //Now solve with tabulation
-
-     public int climbStairs(int n) {
+    public int climbStairs(int n) {
         int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<n+1;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-     }
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+    }
+    public static int helper(int i,int dp[]){
+        if(i==0 ||i==1) return 1;
+        if (i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        dp[i]=helper(i-1,dp)+helper(i-2,dp);
+        return dp[i];
+    }
 }
