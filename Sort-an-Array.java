0e30bde510841fdd0bdb5bf29ad0619d935be0ec
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-07-31 11:48:55 +0530

    [912] [Time Beats: 5.02%] [Memory Beats: 5.10%] - LeetPush

---
+++ Sort-an-Array.java
@@ -0,0 +1,128 @@
+// class Solution {
+
+//     //Bubble sort**********************************************
+
+//     // public int[] sortArray(int[] nums) {
+//     //     int n=nums.length;
+//     //     for(int i=0;i<n-1;i++){
+//     //         for(int j=0;j<n-i-1;j++){
+//     //             if(nums[j]>nums[j+1]){
+//     //                 swap(j,j+1,nums);
+//     //             }
+//     //         }
+//     //     }
+//     //     return nums;
+//     // }
+  
+
+
+// //selection sort******************************************
+// //     public int[] sortArray(int[] nums) {
+// //         int n=nums.length;
+// //         for(int i=0;i<n-1;i++){
+// //             int smallest=i;
+// //             for(int j=i+1;j<n;j++){
+// //                 if(nums[j]<nums[smallest]){
+// //                     smallest=j;
+// //                 }
+// //             }
+// //             swap(i,smallest,nums);
+// //         }
+// //         return nums;
+// // }
+
+// //Insertion Sort /*************************** */
+//     // public int[] sortArray(int[] nums) {
+//     //         int n=nums.length;
+//     //         for(int i=1;i<n;i++){
+//     //             int j=i;
+//     //             while(j>0 && nums[j]<nums[j-1]){
+//     //                 swap(j,j-1,nums);
+//     //                 j--;
+//     //             }
+//     //         }
+//     //         return nums;
+//     // }
+
+
+//     //quick sort
+//       public static void swap(int i,int j,int nums[]){
+//         int temp=nums[i];
+//         nums[i]=nums[j];
+//         nums[j]=temp;
+//     }
+//     public static int partition(int arr[],int start,int end){
+//         int pivot=arr[end];
+//         int i=start-1;
+//         for(int j=start;j<end;j++){
+//             if(arr[j]<pivot){
+//                 i++;
+//                 swap(i,j,arr);
+//             }
+//         }
+//         i++;
+//         swap(i,end,arr);
+//         return i;
+//     }
+//     public static void quicksort(int arr[],int start,int end){
+//         if(start<end){
+//             int pindx=partition(arr,start,end);
+//             quicksort(arr,start,pindx-1);
+//             quicksort(arr,pindx+1,end);
+//         }
+//     }
+
+//     public int[] sortArray(int[] nums) {
+//             int n=nums.length;
+//             quicksort(nums,0,n-1);
+//             return nums;
+//     }
+// }
+
+import java.util.Random;
+
+class Solution {
+    Random rand = new Random();
+
+    public int[] sortArray(int[] nums) {
+        quicksort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quicksort(int[] arr, int start, int end) {
+        if (start < end) {
+            int pivotIndex = randomizedPartition(arr, start, end);
+            quicksort(arr, start, pivotIndex - 1);
+            quicksort(arr, pivotIndex + 1, end);
+        }
+    }
+
+    private int randomizedPartition(int[] arr, int start, int end) {
+        int randIndex = start + rand.nextInt(end - start + 1);
+        swap(arr, randIndex, end); // random pivot
+        return partition(arr, start, end);
+    }
+
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int i = start - 1;
+
+        for (int j = start; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
