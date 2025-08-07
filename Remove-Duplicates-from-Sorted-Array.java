3916cf082d45ce39e4ac3ce459360cf5f19087b5
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-07 10:22:16 +0530

    [26] [Time Beats: 100.00%] [Memory Beats: 61.54%] - LeetPush

--- Remove-Duplicates-from-Sorted-Array.java
+++ Remove-Duplicates-from-Sorted-Array.java
@@ -1,14 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       int n=nums.length;
-       if(n==0) return 0;
-       int i=0;
-       for(int j=1;j<n;j++){
-        if(nums[j]!=nums[i]){
-            i++;
-            nums[i] = nums[j];
+        int n=nums.length;
+        if (n == 0) return 0;
+        int count=1;
+        int i=0,j=1;
+        while(j<n){
+            if(nums[i]!=nums[j]){
+                count++;
+                i++;
+                nums[i]=nums[j];
+                j++;
+            }
+            else j++;
         }
-       }
-       return i+1;
+        return count;
     }
 }
