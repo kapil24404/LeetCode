cafe5b24f94fb5ca78024f09874f79a2f7ee77ce
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-07 11:40:25 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 93.04%] - LeetPush

--- Sort-Colors.java
+++ Sort-Colors.java
@@ -1,27 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-       int low=0;
-       int mid=0;
-       int n=nums.length;
-       int high=n-1;
+        int n=nums.length;
+        int low=0;
+        int mid=0;
+        int high=n-1;
        while(mid<=high){
-        if(nums[mid]==0){
-            swap(mid,low,nums);
-           low++;
-           mid++;
+            if(nums[mid]==0){
+                swap(low,mid,nums);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(mid,high,nums);
+                high--;
+            }
         }
-        else if(nums[mid]==2){
-            swap(mid,high,nums);
-            high--;
-        }
-        else{
-            mid++;
-        }
-       }
     }
-      public void swap(int i,int j,int nums[]){
+    public static void swap(int i,int j,int nums[]){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
-       }
+    }
 }
