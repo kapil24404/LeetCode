5212404a82c6a8bdc6796d0adf54c195d60b3ccf
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-11 15:17:14 +0530

    [145] [Time Beats: 100.00%] [Memory Beats: 66.26%] - LeetPush

--- Binary-Tree-Postorder-Traversal.java
+++ Binary-Tree-Postorder-Traversal.java
@@ -26,4 +26,47 @@ class Solution {
         preorder(root.right,ans);
          ans.add(root.val);
 }
+
+//2 stacks
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //         List<Integer> ans=new ArrayList<>();
+    //         Stack<TreeNode> st1=new Stack<>();
+    //         Stack<TreeNode> st2=new Stack<>();
+    //         if(root==null) return ans;
+    //         st1.push(root);
+    //         while(!st1.isEmpty()){
+    //             root=st1.pop();
+    //             st2.push(root);
+    //             if(root.left!=null) st1.push(root.left);
+    //             if(root.right!=null) st1.push(root.right);
+    //         }
+    //         while(!st2.isEmpty()){
+    //             ans.add(st2.pop().val);
+    //         }
+    //         return ans;
+    // }
+
+    //1 stack
+
+    //  public List<Integer> postorderTraversal(TreeNode root) {
+    //         List<Integer> ans=new ArrayList<>();
+    //         Stack<TreeNode> st=new Stack<>();
+    //         TreeNode curr=root;
+    //         //st.add(curr);
+    //         while(curr!=null &&!st.isEmpty()){
+    //             while(curr!=null){
+    //                 st.add(curr);
+    //                 curr=curr.left;
+    //             }
+    //             if(curr.right==null) {
+    //                 curr=st.pop();
+    //                 ans.add(curr.val);
+    //             }
+    //             else{
+    //                 curr=curr.right;
+    //                 st.add(curr);
+    //             }
+    //         }
+    //         return ans;
+    // }
 }
