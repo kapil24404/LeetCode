3366b39418e134258524f96f4f2655b62e1e3b4a
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-12 11:38:38 +0530

    [3] [Time Beats: 89.33%] [Memory Beats: 43.87%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.java
+++ Longest-Substring-Without-Repeating-Characters.java
@@ -7,10 +7,22 @@ class Solution {
         for(int right=0;right<n;right++){
             char ch=s.charAt(right);
             if(hm.containsKey(ch)){
-                left = Math.max(left, hm.get(ch) + 1);  
+               left=Math.max(left,hm.get(ch)+1);
             }
-            hm.put(ch,right);
-            maxlen=Math.max(maxlen,right-left+1);
+                hm.put(ch,right);
+                int len=right-left+1;
+                maxlen=Math.max(maxlen,len);
+
+            // hm.put(ch,hm.getOrDefault(ch,0)+1);
+            // if(hm.get(ch)==1){
+            //     int len=right-left+1;
+            //     maxlen=Math.max(maxlen,len);
+            // }
+            // else{
+            //     // int leftmost=hm.get(left);
+            //     hm.put(ch,hm.get(ch)-1);
+            //     left++;
+            // }
         }
         return maxlen;
     }
