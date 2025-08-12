fbe153c1001274416928440d4e3ac30e6501f0e6
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-12 11:49:36 +0530

    [1004] [Time Beats: 94.23%] [Memory Beats: 20.94%] - LeetPush

--- Max-Consecutive-Ones-III.java
+++ Max-Consecutive-Ones-III.java
@@ -1,21 +1,17 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int n=nums.length;
-        int maxcount=0;
         int zeroes=0;
         int left=0;
+        int n=nums.length;
+        int maxlen=0;
         for(int right=0;right<n;right++){
-            if(nums[right]==0){
-                zeroes++;
-            }
+            if(nums[right]==0) zeroes++;
             while(zeroes>k){
-                if (nums[left] == 0) {
-                    zeroes--;
-                }
+                 if (nums[left] == 0) zeroes--;
                 left++;
             }
-            maxcount=Math.max(maxcount,right-left+1);
+            maxlen=Math.max(maxlen,right-left+1);
         }
-        return maxcount;
+        return maxlen;
     }
 }
