cdbdc1a1b6270ce85973fe8b6fc3dac59cbace0b
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-12 12:03:52 +0530

    [904] [Time Beats: 72.47%] [Memory Beats: 40.72%] - LeetPush

--- Fruit-Into-Baskets.java
+++ Fruit-Into-Baskets.java
@@ -1,21 +1,21 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int maxlen=0;
-        int left=0;
         int n=fruits.length;
+        int maxlen=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int left=0;
         for(int right=0;right<n;right++){
             hm.put(fruits[right],hm.getOrDefault(fruits[right],0)+1);
-            while (hm.size() > 2)
-            {
-                hm.put(fruits[left], hm.get(fruits[left]) - 1);
-                if (hm.get(fruits[left]) == 0) {
-                    hm.remove(fruits[left]);
+            while(hm.size()>2){
+               int lf = fruits[left];
+                hm.put(lf, hm.get(lf) - 1);
+                if (hm.get(lf) == 0) {
+                    hm.remove(lf);
                 }
-                left++;
+               left++;
             }
             maxlen=Math.max(maxlen,right-left+1);
         }
-       return maxlen; 
+        return maxlen;
     }
 }
