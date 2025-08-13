b158c8917573553da068479e85f6a953098796fa
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-13 13:54:49 +0530

    [1527] [Time Beats: 91.51%] - LeetPush

--- Patients-With-a-Condition.sql
+++ Patients-With-a-Condition.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select * from
-Patients
-where conditions  LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
+Select * 
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
