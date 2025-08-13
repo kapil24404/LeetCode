f160899302637776756d1bc78dd6858010c9350e
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-13 11:22:18 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 47.25%] - LeetPush

--- Unique-Paths.java
+++ Unique-Paths.java
@@ -1,35 +1,12 @@
 class Solution {
-    // public int uniquePaths(int m, int n) {
-    //     int dp[][]=new int[m][n];
-    //     for(int rows[]:dp){
-    //         Arrays.fill(rows,-1);
-    //     }
-    //     return helper(m-1,n-1,dp);
-    // }
-    // public static int helper(int m,int n,int dp[][]){
-    //     if(m==0 && n==0) return 1;
-    //     if(m<0 || n<0) return 0;
-    //     if(dp[m][n]!=-1) return dp[m][n];
-    //     int left=helper(m,n-1,dp);
-    //     int up=helper(m-1,n,dp);
-    //     dp[m][n]=up+left;
-    //     return dp[m][n];
-    // }
-
-
-    //Tabulation
-
-     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int i=0;i<n;i++){
-            dp[0][i]=1;
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m+1][n+1];
+        for(int row[]:dp){
+            Arrays.fill(row,1);
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
         return dp[m-1][n-1];
