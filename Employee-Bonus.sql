1a92f11f039d4162bc76470546f12b8579d76bc6
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-17 12:07:31 +0530

    [577] [Time Beats: 46.93%] - LeetPush

--- Employee-Bonus.sql
+++ Employee-Bonus.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-Select e.name ,b.bonus from Employee as e
-Left JOIN Bonus as b
-ON e.empId=b.empId
-where b.bonus<1000 or b.bonus is NULL;
+Select e.name,b.bonus
+FROM Employee as e
+LEFT JOIN Bonus as b
+ON e.empID=b.empID
+where bonus<1000 OR bonus is NULL;
