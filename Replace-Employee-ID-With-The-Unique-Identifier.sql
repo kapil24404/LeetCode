939e28a28285bbfa0e7c469d82f3d648bfa58ec6
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-17 11:30:18 +0530

    [1378] [Time Beats: 5.01%] - LeetPush

--- Replace-Employee-ID-With-The-Unique-Identifier.sql
+++ Replace-Employee-ID-With-The-Unique-Identifier.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-Select unique_id,name
-FROM Employees as e
-LEFT JOIN 
-EmployeeUNI as f
-ON e.id=f.id;
-
+Select u.unique_id,e.name
+FROM
+Employees as e
+LEFT JOIN EmployeeUNI as u
+ON e.id=u.id;
