09bfd003c2e5493d7260f3c9614e4a8487c64908
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-18 15:12:18 +0530

    [15] [Time Beats: 56.65%] [Memory Beats: 98.70%] - LeetPush

--- 3Sum.java
+++ 3Sum.java
@@ -1,101 +1,25 @@
-// class Solution {
-//     public List<List<Integer>> threeSum(int[] nums) {
-// //*************************************** BRUTE FORCE*****************************************/
-
-
-//         // Set<List<Integer>> res=new HashSet<>();
-//         //  int n=nums.length;
-//         //  for(int i=0;i<n;i++){
-//         //     for(int j=i+1;j<n;j++){
-//         //         for(int k=j+1;k<n;k++){
-//         //             if(nums[i]+nums[j]+nums[k]==0){
-//         //                 List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
-//         //                 Collections.sort(triplet); 
-//         //                 res.add(triplet);
-//         //             }
-//         //         }
-//         //     }
-//         //  }
-//         //  return new ArrayList<>(res);
-
-
-//         //*********Optimal  ************* */
-//         Arrays.sort(nums);
-//         List<List<Integer>> res=new ArrayList<>();
-//         int n=nums.length;
-//         for(int i=0;i<n;i++){
-//             if(i>0 && nums[i]==nums[i-1]) continue;
-//             int j=i+1;
-//             int k=n-1;
-//         while(j<k){
-//             int sum=nums[i]+nums[j]+nums[k];
-//             if(sum==0){
-//                 res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-//                 j++;
-//                 k--;
-//                 while (j < k && nums[j] == nums[j - 1]) j++;
-//                 while (j < k && nums[k] == nums[k + 1]) k--;
-//             }
-//             else if(sum<0){
-//                 j++;
-//             }
-//             else{
-//                 k--;
-//             }
-//         }
-//     }
-//         return res;
-//     }
-// }
-
-//now
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-//*************************************** BRUTE FORCE*****************************************/
-
-
-        // Set<List<Integer>> res=new HashSet<>();
-        //  int n=nums.length;
-        //  for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         for(int k=j+1;k<n;k++){
-        //             if(nums[i]+nums[j]+nums[k]==0){
-        //                 List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
-        //                 Collections.sort(triplet); 
-        //                 res.add(triplet);
-        //             }
-        //         }
-        //     }
-        //  }
-        //  return new ArrayList<>(res);
-
-
-        //*********Optimal  ************* */
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> res=new ArrayList<>();
         int n=nums.length;
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j=i+1;
             int k=n-1;
-        while(j<k){
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum==0){
-                res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                j++;
-                k--;
-                while (j < k && nums[j] == nums[j - 1]) j++;
-                while (j < k && nums[k] == nums[k + 1]) k--;
-            }
-            else if(sum<0){
-                j++;
-            }
-            else{
-                k--;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    j++;
+                    k--;
+                }
+                else if(sum<0) j++;
+                else k--;
             }
         }
-    }
-        return res;
+        return ans;
     }
 }
