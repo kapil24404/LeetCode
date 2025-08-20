ef88cce9bef34a188fdac000366eeb3465ec39d4
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-20 16:57:41 +0530

    [242] [Time Beats: 27.60%] [Memory Beats: 6.75%] - LeetPush

--- Valid-Anagram.java
+++ Valid-Anagram.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
         HashMap<Character,Integer> hm=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            hm.put(ch,hm.getOrDefault(ch,0)+1);
+        for(char i:s.toCharArray()){
+            hm.put(i,hm.getOrDefault(i,0)+1);
         }
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(!hm.containsKey(ch)) return false;
-            else hm.put(ch,hm.get(ch)-1);
+        for(char j:t.toCharArray()){
+            if(hm.containsKey(j)){
+                hm.put(j,hm.get(j)-1);
+            }
+            else return false;
         }
         for(Map.Entry<Character,Integer> ans:hm.entrySet()){
-            if(ans.getValue()>0) return false;
+            if(ans.getValue()!=0) return false;
         }
         return true;
     }
