754625fc2e2d759188b36b0158bf0ab12e60c43c
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-25 14:37:35 +0530

    [152] [Time Beats: 6.98%] [Memory Beats: 7.48%] - LeetPush

--- Maximum-Product-Subarray.java
+++ Maximum-Product-Subarray.java
@@ -1,22 +1,15 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        int maxProduct = Integer.MIN_VALUE;
 
-        int maxProd = nums[0], minProd = nums[0], result = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                int temp = maxProd;
-                maxProd = minProd;
-                minProd = temp;
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = i; j < n; j++) {
+                product *= nums[j];
+                maxProduct = Math.max(maxProduct, product);
             }
-
-            maxProd = Math.max(nums[i], maxProd * nums[i]);
-            minProd = Math.min(nums[i], minProd * nums[i]);
-
-            result = Math.max(result, maxProd);
         }
-
-        return result;
+        return maxProduct;
     }
 }
