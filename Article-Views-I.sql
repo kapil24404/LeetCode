96fe437053407d811e16c62a6f1bceafece6b9a8
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-08-26 14:52:42 +0530

    [1148] [Time Beats: 77.27%] - LeetPush

--- Article-Views-I.sql
+++ Article-Views-I.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-Select 
- DISTINCT author_id as id
-FROM 
-Views
-WHERE 
-author_id=viewer_id
-ORDER BY author_id;
+Select author_id as id
+from Views
+where author_id=viewer_id
+GROUP BY author_id
+ORDER BY author_id asc;
