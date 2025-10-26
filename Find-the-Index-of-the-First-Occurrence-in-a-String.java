c0d0f0d923bcb63c7261a705f6d88ff7a3fd6633
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-10-26 11:06:33 +0530

    [28] [Time Beats: 100.00%] [Memory Beats: 52.07%] - LeetPush

--- Find-the-Index-of-the-First-Occurrence-in-a-String.java
+++ Find-the-Index-of-the-First-Occurrence-in-a-String.java
@@ -2,12 +2,16 @@ class Solution {
     public int strStr(String haystack, String needle) {
      int m=haystack.length();
      int n=needle.length();
-     if(m<n) return -1;
      for(int i=0;i<=m-n;i++){
-        if(haystack.substring(i,i+n).equals(needle)){
-            return i;
+        for(int j=0;j<n;j++){
+            if(haystack.charAt(i+j)!=needle.charAt(j)){
+                break;
+            }
+            if(j==n-1){
+                return i;
+            }
         }
-     }
+     }   
      return -1;
     }
 }
