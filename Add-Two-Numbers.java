c3a36e06496322cba87454f282f40b00643827fe
Kapil <157003808+kapil24404@users.noreply.github.com>
2025-10-27 09:22:37 +0530

    [2] [Time Beats: 100.00%] [Memory Beats: 30.81%] - LeetPush

--- Add-Two-Numbers.java
+++ Add-Two-Numbers.java
@@ -10,10 +10,10 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy=new ListNode(-1);
-        ListNode curr=dummy;
+        ListNode dummy=new ListNode(100);
+        ListNode res=dummy;
         int carry=0;
-        while(l1!=null || l2!=null||carry!=0){
+        while(l1!=null || l2!=null ||carry!=0 ){
             int sum=carry;
             if(l1!=null){
                 sum+=l1.val;
@@ -24,8 +24,8 @@ class Solution {
                 l2=l2.next;
             }
             carry=sum/10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
+            res.next=new ListNode(sum%10);
+            res=res.next;
         }
         return dummy.next;
     }
